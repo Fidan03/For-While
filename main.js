cb63bf4d878b2a8a66c2b3b8e9826afb785bce98
Fidan <90522939+Fidan03@users.noreply.github.com>
2025-01-18 23:09:14 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,130 @@
+
+
+//Task 1
+//let a = prompt("Enter first number: ");
+//let b = prompt("Enter second number: ");
+//let c = prompt("Enter third number: ");
+
+//if (a > b && a > c) {
+//    console.log(`the larges number is ${a}`);
+//} else if (b > a && b > c) {
+//    console.log(`the largest number is ${b}`);
+//} else {
+//    console.log(`the largest number is ${c}`);
+//}
+
+
+
+//Task 2
+year = prompt("Enter a year: ")
+if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    console.log("Artiq il");
+} else {
+    console.log("Artiq il deil");
+}
+    
+
+
+
+
+
+//Task 3
+//let grade = prompt("enter the grade: ");
+
+//if (grade >= 90) {
+//    console.log ("90+ is A");
+//} else if (grade >= 80 && grade <= 89) {
+//    console.log ("80-89 is B");
+//} else if (grade >= 70 && grade <= 79) {
+//    console.log ("70-79 is C");
+//} else if (grade >= 60 && grade <= 69) {
+//    console.log ("60-69 is D");
+//} else {
+//    console.log("less than 60 is F");
+//}
+
+
+
+
+
+//Task 4
+// let num = prompt("Enter the number: ")
+
+//if (num % 2 === 0 ) {
+//    console.log ("Reqem cutdur")
+//} else {
+//    console.log ("Reqem tekdir")
+//}
+
+
+
+
+
+//Task 5
+//for (i=1; i <= 50; i++) {
+//    if (i % 3 === 0 && i % 5 === 0) {
+//        console.log("FizzBuzz");
+//    } else if (i % 5 === 0) {
+//        console.log("Buzz");
+//    } else if (i % 3 === 0) {
+//        console.log ("Fizz")
+//    } else {
+//        console.log (i)
+//    }
+//}
+
+
+
+
+
+//Task 6
+//num = prompt("Enter a number: ")
+
+//for (i = 1; i <= 10; i++) {
+//   console.log(`${num} x ${i} = ${num * i}`)
+//}
+
+
+
+
+
+// Task 7
+//let num = prompt("Enter a number: ");
+//let sum = 0;
+
+//for ( i = 1; i <= num; i++) {
+//    sum += i;
+//}
+//console.log(`the sum is equal ${sum}`);
+
+
+
+
+
+
+//Task 8
+//let a = prompt("Enter first number: ");
+//let b = prompt("Enter second number: ");
+//let c = prompt("Enter operator: ")
+
+//switch(c) {
+//    case "+":
+//        console.log(`${a} + ${b} = ${a + b}`)
+//        break
+//    case "-":
+//        console.log(`${a} - ${b} = ${a - b}`)
+//        break
+//    case "/":
+//        console.log(`${a} / ${b} = ${a / b}`)
+//        break
+//    case "*":
+//        console.log(`${a} * ${b} = ${a * b}`)
+//        break
+//    default:
+//        console.log("invalid opearator.")
+//}
+
+
+
+
+
